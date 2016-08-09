088e6ebf095161e8ad1db479ba760447d8323dff
rufer7 <marc.rufer@d-fens.net>
2016-08-09 17:02:15 +0200

    Assembly version moved to separate file which then got linked to every project to allow changing version in one place

--- src/biz.dfch.CS.Appclusive.Scheduler.Public.Tests/Properties/AssemblyInfo.cs
+++ src/biz.dfch.CS.Appclusive.Scheduler.Public.Tests/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cc02eb84-3073-4e2f-ba16-0f3dbe9bc4cd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.7.*")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
